e6f65380b57501afe0e85b27f1a6fbab4a358d5e
Andre Gugliotti <andre@gugliotti.com.br>
2019-01-19 10:37:22 +0100

    Adds basic config files

---
+++ src/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Module registration file
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Gugliotti_News',
+    __DIR__
+);
