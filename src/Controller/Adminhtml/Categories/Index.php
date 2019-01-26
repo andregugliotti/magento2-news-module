d4be74d9d9ab04b8aa6fa735fe3fa38048566649
Andre Gugliotti <andre@gugliotti.com.br>
2019-01-26 14:04:03 +0100

    Adds basis for the category backend grid

---
+++ src/Controller/Adminhtml/Categories/Index.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Gugliotti News
+ */
+namespace Gugliotti\News\Controller\Adminhtml\Categories;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ *
+ * Gugliotti News Backend Categories Index Controller.
+ * @author Andre Gugliotti <andre@gugliotti.com.br>
+ * @version 0.1.0
+ * @license GNU General Public License, version 3
+ * @package Gugliotti\News\Controller\Adminhtml\Categories
+ */
+class Index extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Gugliotti_News::categories';
+
+    /**
+     * @var bool|PageFactory
+     */
+    protected $resultPageFactory = false;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * execute
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        //$resultPage->setActiveMenu('Gugliotti_News::categories');
+        $resultPage->addBreadcrumb(__('CMS'), __('CMS'));
+        $resultPage->addBreadcrumb(__('Manage Categories'), __('Manage Categories'));
+        $resultPage->getConfig()->getTitle()->prepend(__('News Categories'));
+
+        return $resultPage;
+    }
+}
